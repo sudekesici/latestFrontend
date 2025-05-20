8d0d7270319a1a4fdaeb1514c6b37dc1307c1f90
sudekesici <sudeekesicii@gmail.com>
2025-05-21 01:10:59 +0300

    f

--- src/ChatBot/ChatBot.jsx
+++ src/ChatBot/ChatBot.jsx
@@ -11,7 +11,7 @@ import axios from "axios";
 import "./ChatBot.css";
 
 const ChatBot = () => {
-  const windowRef = useRef(null);
+  const windowRef = useRef(null); 
 
   const [isOpen, setIsOpen] = useState(false);
   const [messages, setMessages] = useState([
